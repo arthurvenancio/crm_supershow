a70f987364151d929835c4d3e8318953ff85d66a
Arthur Venâncio <arthurmvenancio@gmail.com>
2024-04-28 18:17:17 -0300

    first commit

---
+++ JS/indicadores.js
@@ -0,0 +1,39 @@
+import db from "../Servidor/db.mjs"
+let lista_indicadores=db.dados_indicador
+
+lista_indicadores.sort(function(a, b) {
+    if(a.valor < b.valor) {
+      return -1;
+    } else {
+      return true;
+    }
+  });
+
+function criar_indicador(info_indicador){
+
+    const botao_indicador=document.createElement('button')
+    botao_indicador.classList.add('botao_indicador')
+    botao_indicador.classList.add('botao_2')
+
+    if(info_indicador.valor<0){
+        botao_indicador.style.borderColor='var(--vermelho)'
+    }
+
+    const valor_botao_indicador=document.createElement('h1')
+    valor_botao_indicador.classList.add('titulo')
+    valor_botao_indicador.innerHTML=info_indicador.valor+'%'
+
+    const nome_botao_indicador=document.createElement('h2')
+    nome_botao_indicador.classList.add('subtitulo')
+    nome_botao_indicador.innerHTML=info_indicador.nome
+
+    botao_indicador.appendChild(valor_botao_indicador)
+    botao_indicador.appendChild(nome_botao_indicador)
+
+    document.querySelector('.lista_indicadores').appendChild(botao_indicador)
+    
+}
+
+lista_indicadores.forEach(function(info){
+    criar_indicador(info)
+})
---
+++ Servidor/db.mjs
@@ -0,0 +1,21 @@
+const dados_indicador=[{
+    nome:"Indicador",
+    valor:0
+},
+{
+    nome:"Indicador",
+    valor:10
+},
+{
+    nome:"Indicador",
+    valor:-10
+},{
+    nome:"Indicador",
+    valor:-5
+}]
+
+const db={
+    dados_indicador
+}
+
+export default db
