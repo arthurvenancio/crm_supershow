2841159a2a59bf9dd944429717b2830d3e0486b2
Arthur Venâncio <arthurmvenancio@gmail.com>
2024-04-28 21:42:32 -0300

    titulo do quadro de ações concluído

---
+++ JS/quadro_de_acoes.js
@@ -0,0 +1,6 @@
+import db from "../Servidor/db.mjs";
+
+const params = new URLSearchParams(window.location.search);
+const indicador=params.get('indicador')
+
+document.querySelector('.titulo_header').innerHTML=indicador
